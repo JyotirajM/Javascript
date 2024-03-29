a0c905f2f72f995c26e02c8a06342f479dff3dca
Jyotiraj Mahanta <92878273+JyotirajM@users.noreply.github.com>
2024-03-29 16:20:28 +0000

    commit -m "Add variable and datatypes"

---
+++ 1_basics/datatypes.js
@@ -0,0 +1,7 @@
+"use strict";//treat all js code as newer version
+
+//alert("hello")//we are using node js not browser
+
+console.log(6*4);
+
+console.log("Hitesh");
---
+++ 1_basics/variable.js
@@ -0,0 +1,21 @@
+const accountId=100
+let accountEmail ="hgahgs@gmail.com"
+var accountPassword = "12345"
+accountCity="BBSR"
+let accountstate;
+
+
+
+
+accountEmail="gasgh@hgh.com"
+accountPassword="12341"
+accountCity="KJR"
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountstate])
