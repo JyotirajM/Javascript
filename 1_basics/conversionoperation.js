5279281a11099db49c318721b4e235a551fc83a6
Jyotiraj Mahanta <92878273+JyotirajM@users.noreply.github.com>
2024-04-09 06:00:00 +0000

    comparisons in js

--- 1_basics/conversionoperation.js
+++ 1_basics/conversionoperation.js
@@ -1,25 +1,57 @@
-let score =false
+// let score =false
 
-console.log(typeof score);
+// console.log(typeof score);
 
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
-//"33" =>33
-//"33abc" =>NaN
-//true=>1; false=>0
+// //"33" =>33
+// //"33abc" =>NaN
+// //true=>1; false=>0
 
-let isLoggedIn="Hitesh"
-let booleanIsLoggedIn=Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// let isLoggedIn="Hitesh"
+// let booleanIsLoggedIn=Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
 
-//1=>true; 0=>false
-//""=>false;
-// "Hitesh"=>true
+// //1=>true; 0=>false
+// //""=>false;
+// // "Hitesh"=>true
 
-let someNumber=33
-let stringNumber=String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber=33
+// let stringNumber=String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//************ Operations***********//
+let value=33
+let negValue=-value;
+//console.log(negValue);
+
+// console.log(3+3);
+// console.log(3-3);
+// console.log(3*3);
+// console.log(3/3);
+// console.log(3**3);
+// console.log(3%3);
+
+let str1="Hello";
+let str2="Jyoti";
+
+let str3=str1+str2;
+// console.log(str3);
+
+// console.log("1"+3);
+// console.log(1+"3");
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3;
+
+num1=num2=num3=2+2
+
+let gameCounter=100;
+
+gameCounter++;
+console.log(gameCounter);
